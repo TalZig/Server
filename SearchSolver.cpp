c4685baded774f2f888638d8676bf68feac992ed
yoelsherwin <yoelsherwin@gmail.com>
2020-01-12 10:25:17 +0200

    still fixing2

--- SearchSolver.cpp
+++ SearchSolver.cpp
@@ -4,7 +4,8 @@
 
 #include "SearchSolver.h"
 SearchSolver::SearchSolver(iSearcher<Point> *searcher) : searcher_(searcher) {}
+
 string SearchSolver::solve(Matrix problem) {
   State<Point>* goal = this->searcher_->search(problem);
-  return problem.traceBack(problem.getGoalState(), goal);
+  return problem.traceBack(problem.getInitialState(), problem.getGoalState());
 }
