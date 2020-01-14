cbc09b5231f95434552d599437fb339fd29fd5b2
yoelsherwin <yoelsherwin@gmail.com>
2020-01-14 14:06:40 +0200

    started impkementing parallel, only copied from serial

---
+++ MyParallelServer.h
@@ -0,0 +1,78 @@
+//
+// Created by yoel on 14/01/2020.
+//
+
+#ifndef SERVER__MYPARALLELSERVER_H_
+#define SERVER__MYPARALLELSERVER_H_
+#include <thread>
+#include "server_side.h"
+#include <string.h>
+template<typename Problem, typename Solution>
+void start(int port, ClientHandler *c) {
+  int s = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv;
+  serv.sin_addr.s_addr = INADDR_ANY;
+  serv.sin_port = htons(port);
+  serv.sin_family = AF_INET;
+  bool *isTimeOut = new bool;
+  *isTimeOut = false;
+  timeval timeout;
+  timeout.tv_sec = 50;
+  timeout.tv_usec = 0;
+
+  bind(s, (sockaddr *) &serv, sizeof(serv));
+  setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout));
+
+  while (!server_side::GlobalShouldStop) {
+    try {
+      int new_sock;
+      listen(s, 20000);
+      struct sockaddr_in client;
+      socklen_t clilen = sizeof(client);
+      new_sock = accept(s, (struct sockaddr *) &client, &clilen);
+      cout << "Server connected" << endl;
+      if (*isTimeOut) {
+        if (new_sock < 0) {
+          if (errno == EWOULDBLOCK) {
+            cout << "timeout of Serial!" << endl;
+            break;
+          } else {
+            perror("other error");
+            break;
+          }
+        }
+      }
+      c->handleClient(new_sock);
+
+/*        if (new_sock >= 0) {
+          thread *t = new thread(serial, clientHandler, new_sock, isTimeOut);
+          t->join();
+          delete t;
+          *isTimeOut = true;
+        }*/
+    } catch (...) {
+      cout << "connection with client stopped" << endl;
+    }
+  }
+  delete isTimeOut;
+}
+
+template<typename Problem, typename Solution>
+class MyParallelServer : server_side::Server<Problem, Solution> {
+  thread thr1;
+  bool shouldStop = false;
+ public:
+  void open(int port, ClientHandler *c) override {
+    server_side::GlobalShouldStop = false;
+    thread thr(start<Problem, Solution>, port, c);
+    thr.join();
+  }
+  void stop() override {
+    server_side::GlobalShouldStop = true;
+    if (this->thr1.joinable()) {
+      this->thr1.join();
+    }
+  }
+};
+
+#endif //SERVER__MYPARALLELSERVER_H_
